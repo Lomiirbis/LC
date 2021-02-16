3edb6dd931a213750158fcb2004c5220fdc6a67f
lomi <lomi@bk.ru>
2021-02-16 16:14:01 +0300

    Les06
    1,2

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    ofstream f11 ("H1.txt");
+    char c1[] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    f11 << c1;
+    f11.close();
+    ofstream f12 ("H2.txt");
+    char c2[] = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
+    f12 << c2;
+    f12.close();
+    */
+
+    /*
+    ifstream f21 ("H1.txt");
+    char* buf = new char[100];
+    char* buf2 = new char[100];
+    f21 >> buf;
+    f21.close();
+    ifstream f22 ("H2.txt");
+    f22 >> buf2;
+    f22.close();
+    ofstream f23 ("H3.txt");
+    f23 << buf << endl << buf2;
+    f23.close();
+    */
+
+    return 0;
+}
