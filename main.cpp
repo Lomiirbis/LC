9ea61ec84812f32475f5cc0cc79b7768e757ee3c
lomi <lomi@bk.ru>
2021-02-19 13:27:23 +0300

    Урок 07
    на сколько понял ТЗ :(

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#define LES71(x, y) ((0 <= (x) && (x < y)) ? 1 : 0)
+#define SIZE 4
+#define LES72(arr, a) std::cout << *arr[a] <<std::endl;
+
+
+int main()
+{
+/*
+    int x, y;
+    std::cout << " input X: ";
+    std::cin >> x;
+    std::cout << " input Y: ";
+    std::cin >> y;
+    if ((LES71(x, y)) == 1)
+        std::cout << "Yes";
+    else
+        std::cout << "No";
+*/
+
+/*
+    int a = 3;
+    int arr[SIZE];
+    arr[0] = 10;
+    arr[1] = 5;
+    arr[3] = 24;
+    LES72(&arr, a)
+*/
+
+
+
+
+
+    return 0;
+}
