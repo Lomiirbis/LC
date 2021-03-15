e3c16be75b1d598b054e04c0f824d6f5f7ae8bcc
Lomiirbis <75953013+Lomiirbis@users.noreply.github.com>
2021-03-15 22:15:39 +0300

    Lesson02

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    printf("Привет, мир\n");
+    return 0;
+}
